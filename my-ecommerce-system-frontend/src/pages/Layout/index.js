32cf613e094c3fa5a840da9f2e9a22baca0252a3
Zimeng-Zhao <zmengzhao@foxmail.com>
2024-02-03 15:01:39 -0500

    route switching by clicking menu

--- my-ecommerce-system-frontend/src/pages/Layout/index.js
+++ my-ecommerce-system-frontend/src/pages/Layout/index.js
@@ -19,29 +19,37 @@ import {
   LogoutOutlined,
 } from '@ant-design/icons'
 import './index.scss'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 const { Header, Sider } = Layout
-
+//菜单参数
 const items = [
   {
     label: '首页',
-    key: '1',
+    key: '/',
     icon: <HomeOutlined />,
   },
   {
     label: '文章管理',
-    key: '2',
+    key: '/article',
     icon: <DiffOutlined />,
   },
   {
     label: '创建文章',
-    key: '3',
+    key: '/publish',
     icon: <EditOutlined />,
   },
 ]
 
 const GeekLayout = () => {
+  const navigate = useNavigate();
+    const onMenuClick = (route) =>{//点左边的菜单标签 返回对应得key 右边跳转
+        console.log('菜单被点击了', route)
+        //接收路由地址,为什么是key？可以从打印出的route看到 此时地址存在key属性里
+        const path= route.key;
+        //用hook函数导航到想去的route
+        navigate(path);
+    }
   return (
     <Layout>
       <Header className="header">
@@ -61,7 +69,8 @@ const GeekLayout = () => {
             mode="inline"
             theme="dark"
             defaultSelectedKeys={['1']}
-            items={items}
+            onClick={onMenuClick}
+            items={items}//遍历上面的数组生成了左侧菜单
             style={{ height: '100%', borderRight: 0 }}></Menu>
         </Sider>
         <Layout className="layout-content" style={{ padding: 20 }}>
